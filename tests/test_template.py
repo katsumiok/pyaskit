980ab23de5ea01af803c390e6656edab462c2d3e
katsumiok <50365735+katsumiok@users.noreply.github.com>
2023-09-03 16:33:29 -0400

    Add a test case

--- tests/test_template.py
+++ tests/test_template.py
@@ -6,6 +6,8 @@ class TestTemplate(unittest.TestCase):
     def test_extract_variables(self):
         self.assertEqual(template.extract_variables("{{a }} {{a }}"), ["a"])
         self.assertEqual(template.extract_variables("{{ a }} {{ b }}"), ["a", "b"])
+        with self.assertRaises(ValueError):
+            template.extract_variables("{{ a + b }}")
 
     def test_convert_template(self):
         self.assertEqual(template.convert_template("{{ a }}"), "'a'")
