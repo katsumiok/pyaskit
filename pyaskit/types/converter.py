fa97c0f6d431ea39469bbb750884a1dfe6d94a09
katsumiok <50365735+katsumiok@users.noreply.github.com>
2023-09-07 00:09:30 -0400

    Replace get_origin/get_args with getattr

--- pyaskit/types/converter.py
+++ pyaskit/types/converter.py
@@ -1,4 +1,4 @@
-from typing import get_origin, get_args, Union
+from typing import Union
 #from typing import Literal
 import pyaskit.types as t
 
@@ -12,16 +12,18 @@ def convert_type(x):
         return t.bool
     elif x == str:
         return t.str
-    origin = get_origin(x)
+    #origin = get_origin(x)
+    #args = get_args(x)
+    origin = getattr(x, '__origin__', None)
+    args = getattr(x, '__args__', ())
     if origin is list:
-        return t.list(convert_type(get_args(x)[0]))
+        return t.list(convert_type(args[0]))
     elif origin is tuple:
-        return t.tuple(*[convert_type(item_type) for item_type in get_args(x)])
+        return t.tuple(*[convert_type(item_type) for item_type in args])
     elif origin is Union:
-        args = get_args(x)
         return t.union(*[convert_type(arg) for arg in args])
     # elif origin is Literal:
     #     return t.literal(*get_args(x))
     elif origin is dict:
-        return t.record(convert_type(get_args(x)[0]), convert_type(get_args(x)[1]))
+        return t.record(convert_type(args[0]), convert_type(args[1]))
     return x
