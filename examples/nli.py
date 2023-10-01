a099d8573d3d8f39063b9113f45c16d026529702
katsumiok <50365735+katsumiok@users.noreply.github.com>
2023-10-01 17:18:17 -0400

    Add an NLI example

--- examples/nli.py
+++ examples/nli.py
@@ -4,10 +4,19 @@ import pyaskit.types as t
 
 nli = define(
     t.literal("entailment", "neutral", "contradiction"),
-    'What is the relationship between {{s1}} and {{s2}}?')
+    "What is the relationship between {{s1}} and {{s2}}?",
+)
 
-result = nli("The dog is playing with the ball", "There is a dog playing with a ball.")
-print(result)
-print(nli.reason)
+pairs = [
+    ("The dog is playing with the ball", "There is a dog playing with a ball."),
+    ("The dog is playing with the ball", "The cat is playing with the ball."),
+    ("The dog is playing with the ball", "The dog is not playing with the ball.")
+]
 
-
+for s1, s2 in pairs:
+    result = nli(s1, s2)
+    print("---")
+    print(s1)
+    print(s2)
+    print(result)
+    print(nli.reason)
