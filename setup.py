885cbe963e7830cb4545ced47209e9829c41a53a
Katsumi Okuda <okuda@csail.mit.edu>
2025-01-11 15:58:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyaskit",
-    version="1.4.4",
+    version="1.4.5",
     packages=find_packages(),
     description="AskIt: Unified programming interface for programming with large language models (GPT-3.5, GPT-4, Gemini, Claude, COHERE, Llama 2)",
     long_description=open("README.md").read(),
